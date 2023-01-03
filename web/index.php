9fafa1e7f3917b6d1db4aacd0ed7863572acd788
plizhelpme <21001082@myrp.edu.sg>
2023-01-03 12:36:13 +0800

    initial commit

---
+++ web/index.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "Hello World!";
+
+{
+    "name": "vendor_name/heroku",
+    "description": "description",
+    "minimum-stability": "stable",
+    "license": "proprietary",
+    "authors": [
+      {
+        "name": "Ng Yu Xuan",
+        "email": "21001082@myrp.edu.sg"
+      }
+    ],
+    "require": {
+    }
+  }
