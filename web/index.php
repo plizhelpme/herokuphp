6fb89e2391e1bb12097c6eb2e4bcbdd7a1a2ccff
plizhelpme <21001082@myrp.edu.sg>
2023-01-03 13:06:16 +0800

    part 2

--- web/index.php
+++ web/index.php
@@ -1,18 +1,3 @@
 <?php
 
 echo "Hello World!";
-
-{
-    "name": "vendor_name/heroku",
-    "description": "description",
-    "minimum-stability": "stable",
-    "license": "proprietary",
-    "authors": [
-      {
-        "name": "Ng Yu Xuan",
-        "email": "21001082@myrp.edu.sg"
-      }
-    ],
-    "require": {
-    }
-  }
